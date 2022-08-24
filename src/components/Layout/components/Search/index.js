2a3e773835559931150fbf6d9df921de5b38cb0a
Your Name <tkvinhluat.com>
2022-08-24 10:37:28 +0700

    xử lí chỉ request api khi search xong

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -8,6 +8,7 @@ import classNames from 'classnames/bind';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
@@ -18,23 +19,25 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const deboundced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!deboundced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(deboundced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
                 setLoading(false);
             })
             .catch(() => setLoading(false));
-    }, [searchValue]);
+    }, [deboundced]);
 
     const handleClear = () => {
         setSearchValue('');
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+function useDebounce(value, delay) {
+    const [deboundceValue, setDeboundceValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => setDeboundceValue(value), delay);
+        return () => clearTimeout(handler);
+    }, [value]);
+
+    return deboundceValue;
+}
+
+export default useDebounce;
