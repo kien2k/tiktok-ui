a7aedf1f57ad7e5424686772a79b38ba23fe736a
Your Name <tkvinhluat.com>
2022-09-05 20:23:23 +0700

    xây dựng sidebar 1

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,33 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+import {
+    HomeIcon,
+    UsersGroupIcon,
+    LiveIcon,
+    HomeActiveIcon,
+    UsersGroupActiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" icon={<HomeIcon />} to={config.routes.home} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    icon={<UsersGroupIcon />}
+                    to={config.routes.following}
+                    activeIcon={<UsersGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" icon={<LiveIcon />} to={config.routes.live} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
