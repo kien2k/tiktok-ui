89c4c06bc63f5302c82c5d46854591326e0b6a39
Your Name <tkvinhluat.com>
2022-11-07 00:17:40 +0700

    fix icon share

--- src/components/LinkShareList/LinkShareList.js
+++ src/components/LinkShareList/LinkShareList.js
@@ -1,37 +1,34 @@
 import classNames from 'classnames/bind';
-import { faFacebook, faFacebookF, faSquareWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import styles from "./LinkShareList.module.scss"
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import {EmbedIcon} from "~/components/Icons"
+import styles from './LinkShareList.module.scss';
+import { CopyLinkIcon, EmbedIcon, FacebookIcon, SendToIcon, WhatsAppIcon } from '~/components/Icons';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 function LinkShareList() {
     return (
         <div className={cx('wrapper')}>
-            <a href='#' className={cx('share-item')}>
-                <FontAwesomeIcon icon={faFacebook} className={cx('share-icon','black')}/>
+            <a href="#" className={cx('share-item')}>
+                <EmbedIcon className={cx('share-icon')} />
                 <p className={cx('share-text')}>Embed</p>
             </a>
-            <a href='#' className={cx('share-item')}>
-                <FontAwesomeIcon icon={faPaperPlane} className={cx('share-icon','pink')}/>
+            <a href="#" className={cx('share-item')}>
+                <SendToIcon className={cx('share-icon')} />
                 <p className={cx('share-text')}>Send to friends</p>
             </a>
-            <a href='#' className={cx('share-item')}>
-                <FontAwesomeIcon icon={faFacebook} className={cx('share-icon','blue')}/>
+            <a href="#" className={cx('share-item')}>
+                <FacebookIcon className={cx('share-icon')} />
                 <p className={cx('share-text')}>Share to Facebook</p>
             </a>
-            <a href='#' className={cx('share-item')}>
-                <FontAwesomeIcon icon={faSquareWhatsapp} className={cx('share-icon','green')}/>
+            <a href="#" className={cx('share-item')}>
+                <WhatsAppIcon className={cx('share-icon')} />
                 <p className={cx('share-text')}>Share to WhatsApp</p>
             </a>
-            <a href='#' className={cx('share-item')}>
-                <FontAwesomeIcon icon={faFacebook} className={cx('share-icon','pink')}/>
+            <a href="#" className={cx('share-item')}>
+                <CopyLinkIcon className={cx('share-icon')} />
                 <p className={cx('share-text')}>Copy Link</p>
             </a>
-        </div> 
-    )
+        </div>
+    );
 }
 
-export default LinkShareList ;
+export default LinkShareList;
--- src/pages/Home/ContentItem/ContentItem.js
+++ src/pages/Home/ContentItem/ContentItem.js
@@ -1,6 +1,14 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle, faCommentDots, faHeart, faPause, faPlay, faShare, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCheckCircle,
+    faCommentDots,
+    faHeart,
+    faPause,
+    faPlay,
+    faShare,
+    faVolumeHigh,
+} from '@fortawesome/free-solid-svg-icons';
 import { faFlag } from '@fortawesome/free-regular-svg-icons';
 import Tippy from '@tippyjs/react';
 
@@ -13,15 +21,15 @@ import LinkShareList from '~/components/LinkShareList';
 const cx = classNames.bind(styles);
 function ContentItem() {
     const renderLinkShare = (attrs) => (
-        <div className={cx('share-list')} tabIndex="-1" {...attrs} >
+        <div className={cx('share-list')} tabIndex="-1" {...attrs}>
             <PopperWrapper>
-                <LinkShareList/>
+                <LinkShareList />
             </PopperWrapper>
         </div>
-    )
-    
+    );
+
     return (
-        <div >
+        <div>
             <div className={cx('wrapper')}>
                 <Image
                     src="https://rs.n1info.com/wp-content/uploads/2020/11/lionel-mesi-345492-960x625.jpeg"
@@ -52,42 +60,37 @@ function ContentItem() {
                         <div className={cx('video-item')}>
                             <video
                                 className={cx('video')}
-                                src="https://v16-webapp.tiktok.com/a5660843eb5e9ef92b8d0632a8c26805/6364c836/video/tos/alisg/tos-alisg-pve-0037/ooIbUQBZVVfWAXnjj8BDbeZAEb3ChDCgQN1xBt/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=1196&bt=598&cs=0&ds=3&ft=kLO5qy-gZmo0Pz2E3BkVQQjwDiHKJdmC0&mime_type=video_mp4&qs=0&rc=ZDk1NmZkOmdlaTVpNjk2ZEBpM3h4Zmc6ZnJvZzMzODgzNEAtYS01NTM0Xl8xMDFfNC5gYSMvYS5wcjRfYWFgLS1kLy1zcw%3D%3D&l=20221104020645010251065124101B50DD&btag=80000"
+                                src="https://v16-webapp.tiktok.com/f0632f79ab14de08acb1d957f7de76dd/63676fe7/video/tos/maliva/tos-maliva-ve-0068c799-us/4c69a8dac0ac41528eca10fbf85e4a2f/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3096&bt=1548&cs=0&ds=2&ft=kLO5qy-gZmo0PPm83BkVQ1sxDiHKJdmC0&mime_type=video_mp4&qs=0&rc=OjU4OjM3ZTtnZmUzNTRoZUBpajtpdTU6ZjprZjMzZzczNEBeNl5fYzI0XmAxMl9hLTYtYSMtMDVocjQwMWNgLS1kMS9zcw%3D%3D&l=20221106022713010245242205151AC08F&btag=80000"
                             ></video>
                             <p className={cx('video-report')}>
-                                <FontAwesomeIcon icon={faFlag} className={cx('report-icon')}/>  
+                                <FontAwesomeIcon icon={faFlag} className={cx('report-icon')} />
                                 Report
                             </p>
                             <p className={cx('toggle-play-btn')}>
-                                <FontAwesomeIcon icon={faPlay} className={cx('play-icon')}/>  
-                                <FontAwesomeIcon icon={faPause} className={cx('pause-icon')}/>  
+                                <FontAwesomeIcon icon={faPlay} className={cx('play-icon')} />
+                                <FontAwesomeIcon icon={faPause} className={cx('pause-icon')} />
                             </p>
                             <p className={cx('volume-btn')}>
-                                <FontAwesomeIcon icon={faVolumeHigh} className={cx('volume-icon')}/>  
+                                <FontAwesomeIcon icon={faVolumeHigh} className={cx('volume-icon')} />
                             </p>
                         </div>
                         <div className={cx('interaction')}>
                             <div className={cx('interaction-item')}>
                                 <div className={cx('interaction-btn')}>
-                                    <FontAwesomeIcon icon={faHeart} className={cx("icon")}/>
+                                    <FontAwesomeIcon icon={faHeart} className={cx('icon')} />
                                 </div>
                                 <strong>130K</strong>
                             </div>
                             <div className={cx('interaction-item')}>
                                 <div className={cx('interaction-btn')}>
-                                    <FontAwesomeIcon icon={faCommentDots} className={cx("icon")}/>
+                                    <FontAwesomeIcon icon={faCommentDots} className={cx('icon')} />
                                 </div>
                                 <strong>500</strong>
                             </div>
-                            <Tippy
-                                 interactive
-                                 delay={[0, 500]}
-                                 placement="bottom-end"
-                                render={renderLinkShare}
-                                >
+                            <Tippy interactive delay={[0, 500]} placement="top-end" render={renderLinkShare}>
                                 <div className={cx('interaction-item')}>
                                     <div className={cx('interaction-btn')}>
-                                        <FontAwesomeIcon icon={faShare} className={cx("icon")}/>
+                                        <FontAwesomeIcon icon={faShare} className={cx('icon')} />
                                     </div>
                                     <strong>200</strong>
                                 </div>
